301fcf11f9cecdd752d25e5f69f7aa8f64377a9c
knight86 <p.mikulski@wp.pl>
2019-06-09 12:38:33 +0200

    init commit

---
+++ src/main/java/pl/lait/selenium/App.java
@@ -0,0 +1,41 @@
+package pl.lait.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	static WebDriver driver;
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Paweł\\Desktop\\Kurs tester\\chromedriver_win32\\chromedriver.exe");
+        
+       driver = new ChromeDriver();
+       driver.get("http://newtours.demoaut.com");
+      
+       driver.findElement(By.linkText("CONTACT")).click();
+       sleep(2);
+       driver.findElement(By.linkText("SIGN-ON")).click();
+       driver.findElement(By.linkText("REGISTER")).click();
+       driver.findElement(By.linkText("SUPPORT")).click();
+       sleep(3);
+       driver.quit();
+    }
+    
+    public static void sleep(int seconds) {
+    	try {
+    		Thread.sleep(seconds*1000);
+    	} catch (InterruptedException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+}
+}
+}
